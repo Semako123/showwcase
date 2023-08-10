6b2c6627851b80a9d6e09daf0c07a13d2385246b
semako123 <zosusemenoch@gmail.com>
2023-08-10 21:43:43 +0100

    Set up landing page Add image and login container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+"use client";
 import { Container } from "@/components/styles/Container.styles";
-import { H1 } from "@/components/styles/Typography.styles";
+import { StyledImage } from "@/components/styles/Image.styles";
+import { loginImg } from "@/assets";
+import LoginSection from "@/components/styles/LoginSection.style";
 
 const Home = () => {
 	return (
 		<Container>
-			<H1>Hi there, Welcome to your Education Showcase</H1>
-			<p></p>
+			<StyledImage src={loginImg} width={1/2} alt="login" unoptimized/>
+			<LoginSection>
+				
+			</LoginSection>
 		</Container>
 	);
 };
