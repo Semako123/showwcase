b6355f8b0a88cb8322fd95fb7b83ed489d7b9081
semako123 <zosusemenoch@gmail.com>
2023-08-10 22:44:07 +0100

    Rename LoginSection to LoginContainer

---
+++ src/components/styles/LoginContainer.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const LoginContainer = styled.div`
+    display:flex;
+    flex-direction:column;  
+    justify-content:center;
+    text-align:center;
+`
+
+export default LoginContainer
