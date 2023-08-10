d67e5f5898b91e1b1e8969338f12d915bf97aa32
semako123 <zosusemenoch@gmail.com>
2023-08-10 21:38:05 +0100

    Add Theme provider to application Add Global styles to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import StyledComponentsRegistry from "./registry";
+import GlobalStyle from "@/components/styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 export const metadata: Metadata = {
 	title: "Showwcase - Studentfolio",
@@ -13,7 +16,12 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+			<StyledComponentsRegistry>
+				<ThemeProvider theme={theme}>
+					<GlobalStyle />
+					{children}
+				</ThemeProvider>
+			</StyledComponentsRegistry>
 		</html>
 	);
 }
