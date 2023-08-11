1dafc05b48b30ba428d124bb7e44e9c51538e1ae
semako123 <zosusemenoch@gmail.com>
2023-08-11 22:59:31 +0100

    Data mapping into divs Update state on click functionality

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,13 +1,26 @@
+import { EduInfoType } from "@/@types";
 import { Dispatch, SetStateAction } from "react";
 
 const AutoComplete = ({
 	data,
-	setValues,
+	setInputValues,
 }: {
 	data: any;
-	setValues: Dispatch<SetStateAction<any>>;
+	setInputValues: Dispatch<SetStateAction<EduInfoType>>;
 }) => {
-	return <div>AutoComplete</div>;
+	const handleClick = (index: number) => {
+		const uniInfo:any = data[index]
+		setInputValues((prevState) => ({...prevState, name: uniInfo.name, country: uniInfo.country, website:uniInfo.web_pages}))
+	}
+	if (data.length > 0) {
+		return (
+			<div>
+				{data.map((info: any, index: number) => (
+					<div key={index} onClick={() => { handleClick(index) }} >{info.name}</div>
+				))}
+			</div>
+		);
+	} else return <></>;
 };
 
 export default AutoComplete;
