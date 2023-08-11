cb0f424b92ea95a1f1908d9d8fde4ea61b19df59
semako123 <zosusemenoch@gmail.com>
2023-08-11 04:05:09 +0100

    Map education experiences to bookmark link

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,5 +1,17 @@
+import { useEduContext } from "@/context/userContext";
+import Link from "next/link";
+
 const Aside = () => {
-	return <div>This is the aside</div>;
+	const { eduInfo } = useEduContext()!;
+	return (
+		<div>
+			{eduInfo.map((info, index) => (
+				<div key={index}>
+					<Link href={`/dashboard/${index}`}>{info.name}</Link>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Aside;
