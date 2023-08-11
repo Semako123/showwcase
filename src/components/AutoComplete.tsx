dfc1bf2705ea13ec08492527de02360a0d7e0b35
semako123 <zosusemenoch@gmail.com>
2023-08-11 19:22:55 +0100

    Init autocomplete component

---
+++ src/components/AutoComplete.tsx
@@ -0,0 +1,13 @@
+import { Dispatch, SetStateAction } from "react";
+
+const AutoComplete = ({
+	data,
+	setValues,
+}: {
+	data: any;
+	setValues: Dispatch<SetStateAction<any>>;
+}) => {
+	return <div>AutoComplete</div>;
+};
+
+export default AutoComplete;
