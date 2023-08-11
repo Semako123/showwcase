e0357025ac6a3e010914123bb45421a28130acd9
semako123 <zosusemenoch@gmail.com>
2023-08-11 19:26:35 +0100

    Update modal component update layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,40 @@
+"use client";
 import Aside from "@/components/Aside";
+import { useEduContext } from "@/context/userContext";
+import StyledButton from "@/components/styles/Button.styles";
+import { useState } from "react";
+import AddEducation from "@/components/AddEducation";
+import StyledLayout from "@/components/styles/Layout.styles";
+import StyledSection from "@/components/styles/Section.style";
+import StyledHeader from "@/components/styles/Header.styles";
+
 
 export default function DashboardLayout({
 	children,
-	params,
 }: {
 	children: React.ReactNode;
-	params: { name: string };
 }) {
+	const { name } = useEduContext()!;
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 	return (
-		<div>
-			<Aside/>
-			<section>{children}</section>
-		</div>
+		<StyledLayout>
+			<Aside />
+			<StyledSection>
+				<StyledHeader>
+					<h5>
+						Welcome to <span>{name}&apos;s</span> Education{" "}
+						<span>Showcase</span>
+					</h5>
+					<StyledButton
+						onClick={() => {
+							setIsOpen(true);
+						}}>
+						Add New Education
+					</StyledButton>
+				</StyledHeader>
+				<AddEducation setIsOpen={setIsOpen} isOpen={isOpen} />
+				{children}
+			</StyledSection>
+		</StyledLayout>
 	);
 }
