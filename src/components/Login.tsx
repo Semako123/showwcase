a432e31dfe6e5b3085d39ac751fb0422dc728fc6
semako123 <zosusemenoch@gmail.com>
2023-08-11 03:30:43 +0100

    update controlled input via context state Add routing to button

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,27 @@
-"use client"
+"use client";
 import { StyledInput } from "@/components/styles/Input.styles";
 import StyledButton from "@/components/styles/Button.styles";
 import LoginContainer from "@/components/styles/LoginContainer.style";
-import { useState } from "react";
+import { useEduContext } from "@/context/userContext";
+import { useRouter } from "next/navigation";
 
 const Login = () => {
-    const [name, setName] = useState<string>("")
-    const updateName = (e:React.ChangeEvent<HTMLInputElement>) => {
-        setName(e.target.value)
-    }
+	const router = useRouter();
+	const { name, updateName } = useEduContext()!;
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		updateName(e.target.value);
+	};
+    const handleSubmit = () => {
+        console.log("hi")
+		router.push("/dashboard");
+	};
+
 	return (
 		<LoginContainer>
 			<h2>Hi there! Welcome to your Education Showcase</h2>
 			<h5>Type your name and click &quot;Enter&quot; below to ger started</h5>
-			<StyledInput type="text" value={name} onChange={updateName}/>
-			<StyledButton>Enter</StyledButton>
+			<StyledInput type="text" value={name} onChange={handleChange} />
+			<StyledButton onClick={handleSubmit}>Enter</StyledButton>
 		</LoginContainer>
 	);
 };
