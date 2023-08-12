5dda246da0dc3c5f56e7970091d8c54ee967dfd9
semako123 <zosusemenoch@gmail.com>
2023-08-12 19:08:42 +0100

    Fix meta error on deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import type { Metadata } from "next";
+
 import StyledComponentsRegistry from "./registry";
 import GlobalStyle from "@/components/styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
@@ -7,12 +7,6 @@ import { EduContextProvider } from "@/context/userContext";
 import theme from "./theme";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Modal from "react-modal";
-import { ToastContainer } from "react-toastify";
-
-export const metadata: Metadata = {
-	title: "Showwcase - Studentfolio",
-	description: "Showwcase react based test",
-};
 
 export default function RootLayout({
 	children,
@@ -23,14 +17,16 @@ export default function RootLayout({
 	Modal.setAppElement("#app");
 	return (
 		<html lang="en">
-			<head></head>
+			<head>
+				<title>Education Showcase</title>
+				<meta name="description" content="Showwcase react based test" />
+			</head>
 			<body id="app">
 				<StyledComponentsRegistry>
 					<ThemeProvider theme={theme}>
 						<QueryClientProvider client={queryClient}>
 							<EduContextProvider>
 								<GlobalStyle />
-								<ToastContainer />
 								{children}
 							</EduContextProvider>
 						</QueryClientProvider>
