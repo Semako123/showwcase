1ddff2e2d53db2225904ca890f7dd99d66789c77
semako123 <zosusemenoch@gmail.com>
2023-08-12 18:46:31 +0100

    Add ListItem styled component

--- src/components/styles/AutoComplete.styles.ts
+++ src/components/styles/AutoComplete.styles.ts
@@ -9,6 +9,16 @@ const AutoCompleteContainer = styled.div`
     border-radius:10px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     z-index: 10;
+    width:94%;
+    margin-top:10px;
+`
+
+export const ListItems = styled.div`
+    &:hover{
+        cursor:pointer;
+        background-color:${props=>props.theme.color.inactive};
+    }
+    padding:10px 15px;
 `
 
 export default AutoCompleteContainer
