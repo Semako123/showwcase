f33a3a0b88d8b747c4cdfc562f6a5edbfa72b49d
semako123 <zosusemenoch@gmail.com>
2023-08-12 18:33:34 +0100

    Binds Modal / Add Toast container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "styled-components";
 import { EduContextProvider } from "@/context/userContext";
 import theme from "./theme";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Modal from "react-modal";
+import { ToastContainer } from "react-toastify";
 
 export const metadata: Metadata = {
 	title: "Showwcase - Studentfolio",
@@ -18,16 +20,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	const queryClient = new QueryClient();
+	Modal.setAppElement("#app");
 	return (
 		<html lang="en">
-			<head>
-			</head>
-			<body>
+			<head></head>
+			<body id="app">
 				<StyledComponentsRegistry>
 					<ThemeProvider theme={theme}>
 						<QueryClientProvider client={queryClient}>
 							<EduContextProvider>
 								<GlobalStyle />
+								<ToastContainer />
 								{children}
 							</EduContextProvider>
 						</QueryClientProvider>
