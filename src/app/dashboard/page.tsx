872c04a2a90cf05244bf57b3a18cb944fc9c2aff
semako123 <zosusemenoch@gmail.com>
2023-08-12 07:40:47 +0100

    Update page's content Wraps page in styled component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
-const EduList = () => {
-	return <h3>
-		Get started woth showcase Education
-	</h3>
+"use client";
+import { GetStarted } from "@/components/styles/Container.styles";
+
+const Page = () => {
+	return (
+		<GetStarted>
+			<h3>
+				Get started with <span>Education Showcase</span>
+			</h3>
+			<p>
+				Click on &quot;Add Education&quot; to Showcase new Educational
+				Experience
+			</p>
+			<p>Click on an &quot;Educational&quot; Experience to Explore</p>
+		</GetStarted>
+	);
 };
 
-export default EduList;
+export default Page;
