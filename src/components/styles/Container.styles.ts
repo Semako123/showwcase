554551efb9e4e1f2fff7a5f09e8a20683a1e9b43
semako123 <zosusemenoch@gmail.com>
2023-08-12 07:25:47 +0100

    Add Get started styled wrapper Add full prop for container

--- src/components/styles/Container.styles.ts
+++ src/components/styles/Container.styles.ts
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 import { layout, space } from "styled-system";
 
-export const Container = styled.div`
+export const Container = styled.div<{$full?:boolean}>`
 	${layout}
 	${space}
 	display:flex;
-	height:100%;
+	height:${props => props.$full ? "100%" : "auto"}
 `;
+
+export const GetStarted = styled.div`
+	h3{
+		font-weight:bold;
+		font-size:35px;
+	}
+
+	h3 span{
+		color:${props => props.theme.color.secondary};
+	}
+
+	p{
+		color:${props => props.theme.color.darkGrey}
+	}
+`
